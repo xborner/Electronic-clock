706d0e435b070ee767530db284e51b2adad9e45b
xborner <1766507833@qq.com>
2017-06-22 10:57:09 +0800

    test2

---
+++ silent_grep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 match_text filename"
+	exit 1
+fi
+
+match_text=$1
+filename=$2
+grep -q "$match_text" $filename
+
+if [ $? -eq 0 ]; then
+	echo "the text exists in the file"
+else 
+	echo "text does not exist in the file"
+fi
